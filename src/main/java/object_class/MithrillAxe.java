fbc3ed7777e86561a116ad34f46a426d86b43c91
Pierre Talbot <ptalbot@hyc.io>
2022-04-27 00:13:45 +0200

    Add MithrillAxe class.

---
+++ src/main/java/object_class/MithrillAxe.java
@@ -0,0 +1,9 @@
+package object_class;
+
+public class MithrillAxe extends Axe {
+  private boolean madeByDwarf;
+
+  public MithrillAxe(boolean madeByDwarf) {
+    this.madeByDwarf = madeByDwarf;
+  }
+}
